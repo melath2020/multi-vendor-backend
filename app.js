c8ef5277c5b42ced1cb62764c857788628a528bc
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2023-08-05 21:19:13 +0530

    update -err-hndler

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express= require("express");
+const ErrorHandler = require("./utils/ErrorHandler");
 const app= express()
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
 
+app.use(cors({
+  origin: ['https://eshop-tutorial-pyri.vercel.app',],
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(fileUpload({useTempFiles:true}))
 
 
 // config
@@ -9,5 +24,10 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
       path: "config/.env",
     });
   }
+
+
+// it's for ErrorHandling
+app.use(ErrorHandler);
+
 
   module.exports= app;
--- middleware/error.js
+++ middleware/error.js
@@ -29,4 +29,9 @@ module.exports = (err, req, res, next) => {
       }
 
 
+      res.status(err.statusCode).json({
+        success: false,
+        message: err.message,
+      });
+
 }
