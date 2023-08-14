4fcfb6cc2d1b17ad81616a333aa910ef9b505c13
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2023-08-14 23:41:51 +0530

    backendupdate

--- controller/user.js
+++ controller/user.js
@@ -112,6 +112,34 @@ router.post("/activation",catchAsyncErrors(async(req,res,next)=>{
 );
 
 // login user
+router.post("/login-user",catchAsyncErrors(async(req,res,next)=>{
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new ErrorHandler("Please provide the all fields!", 400));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return next(new ErrorHandler("User doesn't exists!", 400));
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return next(
+        new ErrorHandler("Please provide the correct information", 400)
+      );
+    }
+
+    sendToken(user, 201, res);
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+
+}))
 
 
 
