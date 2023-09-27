f1416c16547b9be57968138367bb1844d88e0154
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2023-09-27 11:31:49 +0530

    update-backend

--- controller/user.js
+++ controller/user.js
@@ -161,5 +161,23 @@ router.get("/getuser",isAuthenticated,catchAsyncErrors(async(req,res,next)=>{
   }
 }))
 
+// logout user
+router.get("/logout",isAuthenticated,catchAsyncErrors(async(req,res,next)=>{
+  try{
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    });
+    res.status(201).json({
+      success: true,
+      message: "Log out successful!",
+    });
+  }catch(error){
+    return next(new ErrorHandler(error.message, 500));
+  }
+}))
+
 
 module.exports = router;
